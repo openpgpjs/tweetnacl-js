70b14f23f7e8e52908c2b78e7bcbe6ece018664a
larabr <7375870+larabr@users.noreply.github.com>
2025-03-27 14:40:05 +0100

    TS: fix default export to work with ESM module
    Export assignment cannot be used when targeting ECMAScript modules.

--- nacl.d.ts
+++ nacl.d.ts
@@ -3,7 +3,7 @@
 export as namespace nacl;
 
 declare var nacl: nacl;
-export = nacl;
+export default nacl;
 
 declare namespace nacl {
     export interface BoxKeyPair {
@@ -32,10 +32,10 @@ declare namespace nacl {
     }
 
     namespace boxProps {
-    //     export interface open {
-    //         (msg: Uint8Array, nonce: Uint8Array, publicKey: Uint8Array, secretKey: Uint8Array): Uint8Array | null;
-    //         after(box: Uint8Array, nonce: Uint8Array, key: Uint8Array): Uint8Array | null;
-    //     }
+        // export interface open {
+        //     (msg: Uint8Array, nonce: Uint8Array, publicKey: Uint8Array, secretKey: Uint8Array): Uint8Array | null;
+        //     after(box: Uint8Array, nonce: Uint8Array, key: Uint8Array): Uint8Array | null;
+        // }
 
         export interface keyPair {
             (): BoxKeyPair;
